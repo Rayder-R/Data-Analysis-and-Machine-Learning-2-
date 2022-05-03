1e92e193df610783c8e95c471b4ad6a8360aed3f
Rayder|曾瑞義 <zen2362661@gmail.com>
2022-05-04 00:18:03 +0800

    8

--- 11.py
+++
@@ -1,7 +0,0 @@
-
-import random
-x = random.randrange(1, 60000)
-print(x)
-print(x)
-print(x)
-print(x)
---
+++ 卷積神經網路(CNN)/Keras_Mnist_CNN.py
@@ -0,0 +1,4 @@
+import numpy as np
+from keras.utils import np_utils
+np.random.seed(10)
+from keras.datasets import mnist
