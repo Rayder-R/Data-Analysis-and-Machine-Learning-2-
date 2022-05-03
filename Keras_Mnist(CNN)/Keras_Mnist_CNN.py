f9f05eaf381c85e760000249983d430d8dede8ed
Rayder|曾瑞義 <zen2362661@gmail.com>
2022-05-04 01:11:34 +0800

    9

--- Keras_Mnist(CNN)/Keras_Mnist_CNN.py
+++ Keras_Mnist(CNN)/Keras_Mnist_CNN.py
@@ -2,3 +2,13 @@ import numpy as np
 from keras.utils import np_utils
 np.random.seed(10)
 from keras.datasets import mnist
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Conv2D,MaxPool2D,Dropout,Flatten,Dense
+
+def show_imasge_labels_prediction(images, label, prediction, star_id, num=10):
+    plt.gcf().set_size_inches(12, 14)
+    if num>25: num=25
+    for i in range(0, num):
+        ax = plt.subplot(5,5,1+i)
+        
